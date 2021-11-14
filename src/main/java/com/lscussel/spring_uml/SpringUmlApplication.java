9396132d39ac12ff87973ea13bf94ea3512e0d61
Leonardo Scussel <leonardoscussel@gmail.com>
2021-11-14 18:18:39 -0300

    20211114 1818 Projeto criado

---
+++ src/main/java/com/lscussel/spring_uml/SpringUmlApplication.java
@@ -0,0 +1,13 @@
+package com.lscussel.spring_uml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUmlApplication.class, args);
+	}
+
+}
