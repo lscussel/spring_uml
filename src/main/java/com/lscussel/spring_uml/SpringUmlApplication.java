655688acc103eb110b78679d31b59bb35f7e9136
Leonardo Scussel <leonardoscussel@gmail.com>
2021-11-14 20:48:12 -0300

    20211114 2047 Criando operacao de instanciacao

--- src/main/java/com/lscussel/spring_uml/SpringUmlApplication.java
+++ src/main/java/com/lscussel/spring_uml/SpringUmlApplication.java
@@ -1,13 +1,31 @@
 package com.lscussel.spring_uml;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringUmlApplication {
+import com.lscussel.spring_uml.domain.Categoria;
+import com.lscussel.spring_uml.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringUmlApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringUmlApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));		
+	}	
 
 }
