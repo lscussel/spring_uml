f0ed28ff291bfbd599c82a95bf2de6c3ad54aa73
Leonardo Scussel <leonardoscussel@gmail.com>
2021-11-15 11:37:53 -0300

    20211511 1137 Estado e cidade

---
+++ src/main/java/com/lscussel/spring_uml/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.lscussel.spring_uml.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lscussel.spring_uml.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
